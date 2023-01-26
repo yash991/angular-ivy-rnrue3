bf9b5a3316e14d1f278061bd592aed373ee2de17
yash991 <55062950+yash991@users.noreply.github.com>
2023-01-26 20:14:34 +0000

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,13 +17,13 @@ export class AppComponent implements OnInit {
       { id: 2, name: 'Jan-Niklas' },
     ];
 
-    // const source$ = from(objects)
-    //   .pipe(tap((item) => (item.name = item.name + '_2')))
-    //   .subscribe((x) => console.log(x));
+    const source$ = from(objects)
+      .pipe(tap((item) => (item.name = item.name + '_2')))
+      .subscribe((x) => console.log(x));
 
-    // from([1, 2, 3])
-    // .pipe(map((item) => item + 2))
-    // .subscribe((item) => console.log(item));
+    from([1, 2, 3])
+    .pipe(map((item) => item + 2))
+    .subscribe((item) => console.log(item));
 
     from([1, 2, 3, 9])
       .pipe(switchMap((item) => this.methodWhichReturnsObservable(item)))
